2bdb55b00693519e9835081efc02ccde058128ae
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-10-16 03:18:04 +0300

    created setState and local state for status in Profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,21 +3,22 @@ import React from "react";
 import { Preloader } from "../../common/preloader/Preloader";
 import style from "./ProfileInfo.module.css";
 import usersIcon from './../../../assect/images/usersIcon.png';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
 
   if(!props.profile) {
      return <Preloader />
   }
-  console.log(props.profile);
   return (
     <div>
       <div>
-        <img className={style.img}
+        {/* <img className={style.img}
           src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
           alt="photo"
-        />
+        /> */}
       </div>
+      <ProfileStatus status='Hello my friends'/> 
       <div className={style.discrptionBlock}>
         {props.profile.photos.large ? <img src={props.profile.photos.large} alt="photo" /> : <img src={usersIcon} alt="photo"/>}
         <div>{props.profile.fullName}</div>
