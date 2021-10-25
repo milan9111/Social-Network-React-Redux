2ab60e04f8a6a3985f36286ded7c2963ec69bd54
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-10-26 00:59:56 +0300

    component App with BrowserRouter basename

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ let AppContainer = compose(
    
 
 let SamuraiJSApp = (props) => {
-  return <BrowserRouter>
+  return <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
               <AppContainer /> 
         </Provider>
